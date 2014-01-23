61c0d3f65c603240f6f7e26e4daa44fd8cafdd2e
Andrew Maffei <amaffei@whoi.edu>
2014-01-23 15:53:57 -0500

    got edist_make_instruments.pl working

---
+++ bin/edist_make_instruments.pl
@@ -0,0 +1,51 @@
+#!/Users/drumbeat/perl5/perlbrew/perls/perl-5.18.2/bin/perl
+
+  use strict;
+  use warnings;
+  use Tie::Handle::CSV;
+
+  my $file = "/Users/drumbeat/git/cruise_maint/edist_cfg/instruments_master.csv";
+  my $csv_parser = Text::CSV_XS->new( { 
+     quote_char            => '"',
+     escape_char           => '"',
+     sep_char              => ',',
+     eol                   => $\,
+     always_quote          => 0,
+     quote_space           => 1,
+     quote_null            => 1,
+     quote_binary          => 1,
+     binary                => 0,
+     decode_utf8           => 1,
+     keep_meta_info        => 0,
+     allow_loose_quotes    => 0,
+     allow_loose_escapes   => 0,
+     allow_unquoted_escape => 0,
+     allow_whitespace      => 0,
+     blank_is_undef        => 0,
+     empty_is_undef        => 0,
+     verbatim              => 0,
+     auto_diag             => 0,
+     diag_verbose          => 0,
+     });
+
+  my $fh = Tie::Handle::CSV->new ($file,
+				  header => 1,
+				  csv_parser => $csv_parser );
+
+  #
+  # print instruments.csv header
+  #
+  print "\"DeviceID\",\"DeviceTerm\",\"DeviceCategory\",\"DeviceDefActions\",\"DefaultInstrumentTerm\",\"DeviceDescription\"\n";
+
+  #
+  # print instruments.csv rows
+  # "R2RE/1034","NOAA SCS data acquisition system","R2RE/9010","data acquisition system","Acqsys","NOAA SCS data acquisition system description goes here"
+  while (my $csv_line = <$fh>) {
+      # print $csv_line->{EcfmID} . ":\t" . $csv_line->{EcfmName} . "\n";
+      if ($csv_line->{Edist} eq "Yes") {
+	  # print $csv_line->{EcfmID} . ":\t" . $csv_line->{EcfmName} . "\n";
+          print "\"" . $csv_line->{EcfmID} . ",\"" . $csv_line->{EcfmName} . ",\"" . $csv_line->{EcfmCategory} . ",\"" . $csv_line->{EcfmDefActions} . ",\"" . $csv_line->{EcfmDefName} . ",\"" . $csv_line->{EcfmDesc} . "\"\n";
+      }
+      }
+
+  close $fh;
